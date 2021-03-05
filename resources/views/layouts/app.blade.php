761db1b2da33482555559dec382a6b329dacfa55
elamirKad <elamirkad@gmail.com>
2021-03-05 23:14:12 +0500

    CKEDITOR added and Navbar moved to nav file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,8 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!--<script src="{{ asset('js/app.js') }}" defer></script>-->
+    <script src="//cdn.ckeditor.com/4.16.0/standard/ckeditor.js"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -20,58 +21,14 @@
 </head>
 <body class="bg-gray-800 text-black font-sans">
     <div id="app" >
-        <nav class="p-6 bg-indigo-600 flex justify-between mb-6 text-white">
-            <ul class="flex items-center">
-                <li class="p-3">
-                    <a href="/">Main</a>
-                </li>
-                <li class="p-3">
-                    <a href="/home">Dashboard</a>
-                </li>
-                <li class="p-3">
-                    <a href="/offers">Offers</a>
-                </li>
-            </ul>
-            <!-- Right Side Of Navbar -->
-            <ul class="flex items-center">
-                <!-- Authentication Links -->
-                @guest
-                    @if (Route::has('login'))
-                        <li class="p-3">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                    @endif
-                    
-                    @if (Route::has('register'))
-                        <li class="p-3">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                    @endif
-                @else
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }}
-                        </a>
-
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                             document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                @endguest
-            </ul>
-        </nav>
+        @include('layouts.nav')
 
         <main class="py-4 ">
             @yield('content')
         </main>
     </div>
+    <script>
+        CKEDITOR.replace( 'article-ckeditor' );
+    </script>
 </body>
 </html>
--- resources/views/lots/create.blade.php
+++ resources/views/lots/create.blade.php
@@ -2,27 +2,31 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Create Post</h1>
-    {!! Form::open(['action' => 'App\Http\Controllers\LotsController@store', 'method' => 'POST', 'files' => true, 'ecntype' => 'multipart/form-data']) !!}
-        <div class="form-group">
-            {{Form::label('title', 'Title')}}
-            {{Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title'])}}
+    <div class="justify-center flex">
+        <div class="w-8/12 bg-gray-200 text-black p-6 rounded-lg">
+            <h1>Create Post</h1>
+            {!! Form::open(['action' => 'App\Http\Controllers\LotsController@store', 'method' => 'POST', 'files' => true, 'ecntype' => 'multipart/form-data']) !!}
+                <div class="form-group">
+                    {{Form::label('title', 'Title')}}
+                    {{Form::text('title', '', ['class' => 'form-control', 'placeholder' => 'Title'])}}
+                </div>
+                <div class="form-group">
+                    {{Form::label('address', 'Address')}}
+                    {{Form::text('address', '', ['class' => 'form-control', 'placeholder' => 'Address'])}}
+                </div>
+                <div class="form-group">
+                    {{Form::label('price', 'Price')}}
+                    {{Form::number('price', '', ['class' => 'form-control', 'placeholder' => '100000'])}}
+                </div>
+                <div class="form-group">
+                    {{Form::label('body', 'Body')}}
+                    {{Form::textarea('body', '', ['id' => 'article-ckeditor','class' => 'form-control', 'placeholder' => 'Body Text'])}}
+                </div>
+                <div class="form-group">
+                    {{Form::file('cover_image')}}
+                </div>
+                {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
+            {!! Form::close() !!}
         </div>
-        <div class="form-group">
-            {{Form::label('address', 'Address')}}
-            {{Form::text('address', '', ['class' => 'form-control', 'placeholder' => 'Address'])}}
-        </div>
-        <div class="form-group">
-            {{Form::label('price', 'Price')}}
-            {{Form::number('price', '', ['class' => 'form-control', 'placeholder' => '100000'])}}
-        </div>
-        <div class="form-group">
-            {{Form::label('body', 'Body')}}
-            {{Form::textarea('body', '', ['id' => 'article-ckeditor','class' => 'form-control', 'placeholder' => 'Body Text'])}}
-        </div>
-        <div class="form-group">
-            {{Form::file('cover_image')}}
-        </div>
-        {{Form::submit('Submit', ['class' => 'btn btn-primary'])}}
-    {!! Form::close() !!}
+    </div>
 @endsection
